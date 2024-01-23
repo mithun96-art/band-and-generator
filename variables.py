8d3d90918442e770e5c7d25f5e6c712aad970444
mithun96-art <157538757+mithun96-art@users.noreply.github.com>
2024-01-23 16:58:19 -0500

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,38 @@
+
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 13 21:11:49 2023
+
+@author: iamrs
+"""
+
+# this code for learning about variables
+
+x=5
+y2="mithun"
+print(type(x))
+print(type(y2))
+X1=120.24
+print(X1)
+_x = 10
+print(_x)
+#for = "adsafdfs"
+x = y = z = "Orange"
+print(x,y,z)
+print(x)
+print(y)
+print(z)
+#while
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits[2])
+
+x = "Python"
+y = "is"
+z = "awesom     
+def myfunc():
+  x="not good"
+  print("Python is " + x3)
+
+myfunc()
+carname = "volvo"
